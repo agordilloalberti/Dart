a115e25ec2408891ff55b3e010d409a85f32adc5
Alvarogogal <agorsua0610@g.educaand.es>
2024-11-28 17:07:50 +0100

    .

---
+++ Dart.dart
@@ -0,0 +1,29 @@
+void main() {
+  for (var i = 0; i < 10; i++) {
+    print('hello ${i + 1}');
+  }
+}
+
+
+void generosMusicales(){
+
+  Scanner sc = new Scanner(System.in);
+  Set<String> set = {};
+  int op;
+
+  do{
+
+    op=0;
+
+  }while(op!=0);
+
+  print(set);
+}
+
+// void tareas(){
+//   List list = new ArrayList();
+// }
+
+// void calificaciones(){
+//   Map map = new Map();
+// }
