a7ec499132bf12bd610b64eab9c8cce989ba6ab7
Alvarogogal <agorsua0610@g.educaand.es>
2024-11-28 17:36:50 +0100

    .

--- Dart.dart
+++ Dart.dart
@@ -1,4 +1,3 @@
-import 'dart:developer';
 import 'dart:io';
 
 void main() {
@@ -8,8 +7,7 @@ void main() {
 
 void generosMusicales(){
 
-
-  Set<String> set = {};
+  Set<String> generos = {};
   String? op;
 
   do{
@@ -19,18 +17,18 @@ void generosMusicales(){
     switch(op){
       case "1":
 
-        if(set.length<5) {
+        if(generos.length<5) {
         print('Introduzca el genero a añadir');
         String? genero = stdin.readLineSync();
 
         if(genero==null || genero==''){
           print('Tiene que ingresar un valor');
         }else{
-            if (set.contains(genero)) {
+            if (generos.contains(genero)) {
               print('El genero $genero ya existe');
             } else {
               print('El genero $genero ha sido añadido');
-              set.add(genero);
+              generos.add(genero);
             }
           }
         }else{
@@ -39,6 +37,7 @@ void generosMusicales(){
 
         break;
       case "2":
+        //TODO
         break;
       case "3":
         break;
@@ -50,13 +49,49 @@ void generosMusicales(){
 
   }while(op!=0);
 
-  print(set);
+  print(generos);
 }
 
-// void tareas(){
-//   List list = List.filled(length, fill);
-// }
-//
-// void calificaciones(){
-//   Map map = new Map();
-// }
+void tareas(){
+  List listaTareas = List.empty(growable: true);
+
+  String? op;
+
+  do{
+    print('Seleccione una opción:\n1. Añadir tarea\n2.Finalizar tarea actual\n3.Ver siguiente tarea\n4.Ver todas las tareas\n0.Salir');
+    op=stdin.readLineSync();
+
+    switch(op) {
+      case "1":
+        String? tarea = stdin.readLineSync();
+        if(tarea==null || tarea=='') {
+          print('Debe de ingresar un valor');
+        }else{
+          listaTareas.add(tarea);
+          print('Tarea añadida');
+        }
+        break;
+      case "2":
+        listaTareas.remove(listaTareas.first);
+        print('La tarea actual ha sido eliminado');
+        break;
+      case "3":
+        print(listaTareas.first);
+        break;
+      case "4":
+        print(listaTareas);
+        break;
+      case "0":
+        print('Adios');
+        break;
+      default:
+        print('Opción incorrecta');
+        break;
+
+    }
+  }while(op!="0");
+}
+
+void calificaciones(){
+  Map map = new Map();
+}
