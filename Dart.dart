74b244d437b385b3e374771601ac75ae1c6c0958
Alvarogogal <agorsua0610@g.educaand.es>
2024-11-28 17:22:24 +0100

    .

--- Dart.dart
+++ Dart.dart
@@ -1,19 +1,52 @@
+import 'dart:developer';
+import 'dart:io';
+
 void main() {
-  for (var i = 0; i < 10; i++) {
-    print('hello ${i + 1}');
-  }
+  generosMusicales();
 }
 
 
 void generosMusicales(){
 
-  Scanner sc = new Scanner(System.in);
+
   Set<String> set = {};
-  int op;
+  String? op;
 
   do{
+    print('Seleccione una opción:\n1. Añadir genero\n2.Eliminar genero\n3.Ver todos\n0.Salir');
+    op=stdin.readLineSync();
 
-    op=0;
+    switch(op){
+      case "1":
+
+        if(set.length<5) {
+        print('Introduzca el genero a añadir');
+        String? genero = stdin.readLineSync();
+
+        if(genero==null || genero==''){
+          print('Tiene que ingresar un valor');
+        }else{
+            if (set.contains(genero)) {
+              print('El genero $genero ya existe');
+            } else {
+              print('El genero $genero ha sido añadido');
+              set.add(genero);
+            }
+          }
+        }else{
+          print('No pueden añadirse mas de 5 generos');
+        }
+
+        break;
+      case "2":
+        break;
+      case "3":
+        break;
+      case "0":
+        break;
+      default:
+        break;
+    }
 
   }while(op!=0);
 
@@ -21,9 +54,9 @@ void generosMusicales(){
 }
 
 // void tareas(){
-//   List list = new ArrayList();
+//   List list = List.filled(length, fill);
 // }
-
+//
 // void calificaciones(){
 //   Map map = new Map();
 // }
